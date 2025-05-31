bb8f847fe0002e39a7adcd9b454fb4556aea00e0
Andrii Polubinskyi <polubin@gmail.com>
2025-05-31 13:13:25 +0300

    Added docker for frontend

--- MedicalData.Client/Program.cs
+++ MedicalData.Client/Program.cs
@@ -7,11 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-#if DEBUG
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(@"https://medicaldata-api.onrender.com/") });
-#else
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-#endif
 
 builder.Services.AddFluentUIComponents();
 
